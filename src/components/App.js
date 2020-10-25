366041cf04fc18ede1b56d8f22309dd3b37b3356
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-26 00:14:38 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,36 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
+  const [ballPosition, setBallPosition] = useState({
+    left: x + "px",
+    top: y + "px"
   });
   const reset = () => {};
-  const renderChoice = () => {};
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button className="start" onClick={buttonClickHandler}>
+          Click For One Ball
+        </button>
+      );
+  };
+  const handleClick = (event) => {
+    if (event.keyCode === 37) {
+      //Left
+    } else if (event.keyCode === 38) {
+      //Up
+    } else if (event.keyCode === 39) {
+      //Right
+    } else if (event.keyCode === 40) {
+      //Down
+    }
+  };
+  const buttonClickHandler = () => {
+    setRenderBall(!renderBall);
+    document.addEventListener("keydown", handleClick);
+  };
 
   return (
     <div className="playground">
